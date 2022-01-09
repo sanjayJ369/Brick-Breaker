9bd8ff4ed69e94b2d00872c8509bfcf299072133
realonesc <ryanrmc@gmail.com>
2022-01-09 19:29:30 +0900

    Worked on basic main menu

---
+++ scenes/menu/menu.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_PlayButton_body_entered(body):
+	switch_scene("hi")
+
+func switch_scene(scene):
+	play_transition()
+	get_tree().change_scene("res://scenes/main.tscn")
+		
+func play_transition():
+	# TODO
+	pass
