e7513f2c7d5fbd3487fa85435334741174eb6405
MATRIX336699 <j.sridhar333@gmail.com>
2022-02-04 21:38:40 +0530

    added sand

--- scenes/bricks/sand bricks/sand.gd
+++ scenes/bricks/sand bricks/sand.gd
@@ -2,14 +2,17 @@ extends StaticBody2D
 
 
 export var brick_life = 15
+export(bool) var infinite_balls = false 
 var life = brick_life
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$brick_life.text = str(life)
+	if !infinite_balls:
+		$brick_life.text = str(life)
 func take_life():
-	life -= 1
-	$brick_life.text = str(life)
+	if !infinite_balls:
+		life -= 1
+		$brick_life.text = str(life)
 	if life == 0:
 		queue_free()
 		
